df9878097a8facfa0c3d35c9c5477393b53d1816
alexander_briasco <astewart.is1@gmail.com>
2016-09-09 12:37:50 -0400

    Branch Test 2

--- src/AlexTestBranch.java
+++ src/AlexTestBranch.java
@@ -4,5 +4,6 @@
 public class AlexTestBranch {
     public static void main(String[] args) {
         System.out.println("This is on a separate branch");
+        System.out.println("Testing the merge feature");
     }
 }
